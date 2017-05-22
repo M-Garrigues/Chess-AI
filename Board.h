07af6ccc33e92d6cbd6ffb55e393d70d25e16d83
Mathieu Garrigues <mathieu.garrigues@etu.univ-lyon1.fr>
2017-05-22 16:23:44 +0200

    Pieces added

---
+++ Board.h
@@ -0,0 +1,15 @@
+//
+// Created by Mathieu on 22/05/2017.
+//
+
+#ifndef CHESS_AI_BOARD_H
+#define CHESS_AI_BOARD_H
+
+class Board{
+
+private:
+    char [8][8] tab;
+
+};
+
+#endif //CHESS_AI_BOARD_H
